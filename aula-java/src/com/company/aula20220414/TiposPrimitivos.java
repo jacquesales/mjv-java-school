58f55adcdddc3f62e40b8ec08b484ca512cc1c12
Jacqueline Sales <tendencce@gmail.com>
2022-04-19 13:41:08 -0300

    Add exercicio tipos primitivos - aula20220414

---
+++ aula-java/src/com/company/aula20220414/TiposPrimitivos.java
@@ -0,0 +1,16 @@
+package com.company.aula20220414;
+
+public class TiposPrimitivos {
+
+    public static void main(String[] args) {
+
+        byte vbyte = 123;
+        short vshort = 12345;
+        int vint = 1234567890;
+        long vlong = 1234567890000000000L;
+        double vdouble = 12.33;
+        float pi = 3.14F;
+        char vchar = 'a';
+        boolean vboolean = true; //se não especificado, o valor default é false
+    }
+}
