23c16cbcda4da69e08ad8e587b9cd3035cc97a88
Jacqueline Sales <tendencce@gmail.com>
2022-04-20 20:33:31 -0300

    Mudando valor de BigDecimal para Double e add ToString

--- open-banking/src/main/java/Movimentacao.java
+++ open-banking/src/main/java/Movimentacao.java
@@ -1,15 +1,16 @@
-import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 @Getter
 @Setter
+@ToString
 public class Movimentacao {
 
     private LocalDateTime dataHora;
-    private BigDecimal valor;
+    private Double valor;
     private String cpfCnpj;
     private String nomeCliente;
     private Boolean estorno;
