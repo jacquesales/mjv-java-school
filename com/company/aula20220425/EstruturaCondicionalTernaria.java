fa5cb2045a1074e47b10b88b27416eb6d1245d19
Jacqueline Sales <tendencce@gmail.com>
2022-04-27 09:12:58 -0300

    Deletado (sem uso)

---
+++ com/company/aula20220425/EstruturaCondicionalTernaria.java
@@ -0,0 +1,16 @@
+package com.company.aula20220425;
+
+public class EstruturaCondicionalTernaria {
+    public static void main(String[] args) {
+
+        simuladorDecolagem(100);
+        simuladorDecolagem(5000);
+    }
+
+    //fluxo de atribuição (um valor ou outro); e não comportamental (o fluxo não é redirecionado)
+    static void simuladorDecolagem(int altitude) {
+
+        String estagioVoo = (altitude >= 5000) ? "voo estável" : ((altitude < 5000 && altitude >= 5) ? "subindo..." : "não decolou");
+        System.out.println("Estágio da decolagem: " + estagioVoo);
+    }
+}
