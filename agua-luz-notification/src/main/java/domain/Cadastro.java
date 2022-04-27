976b907afdb0ff634a959a78e0648a69c36917d9
Jacqueline Sales <tendencce@gmail.com>
2022-04-27 09:31:34 -0300

    Entidade Cadastro refatorada

--- agua-luz-notification/src/main/java/domain/Cadastro.java
+++ agua-luz-notification/src/main/java/domain/Cadastro.java
@@ -1,6 +1,5 @@
 package domain;
 
-import domain.enums.TipoPais;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -14,12 +13,5 @@ public class Cadastro {
     private String rg;
     private String nome;
     private long celular;
-    private String logradouro;
-    private int numero;
-    private String complemento;
-    private String bairro;
-    private String cidade;
-    private String uf;
-    private String cep;
-    private TipoPais pais;
+
 }
