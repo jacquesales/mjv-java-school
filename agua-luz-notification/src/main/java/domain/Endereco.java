aeedec2cf9eda2faf1c0e1d41dba714b7dea30da
Jacqueline Sales <tendencce@gmail.com>
2022-04-27 09:31:56 -0300

    Add entidade Endereço

---
+++ agua-luz-notification/src/main/java/domain/Endereco.java
@@ -0,0 +1,21 @@
+package domain;
+
+import domain.enums.TipoPais;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class Endereco {
+
+    private String logradouro;
+    private int numero;
+    private String complemento;
+    private String bairro;
+    private String cidade;
+    private String uf;
+    private String cep;
+    private TipoPais pais;
+}
