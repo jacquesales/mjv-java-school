63a8e40b45e1ff002731037ee9b03ddf36a638cf
Jacqueline Sales <tendencce@gmail.com>
2022-05-05 20:07:05 -0300

    Retirado tipo List da classe Contrato

--- agua-luz-atracao/src/main/java/app/SistemaAguaLuzGeracaoArquivo.java
+++ agua-luz-atracao/src/main/java/app/SistemaAguaLuzGeracaoArquivo.java
@@ -3,18 +3,17 @@ package app;
 import domain.Contrato;
 import repository.ContratoRepository;
 import services.GeradorArquivo;
-import java.util.List;
 
 public class SistemaAguaLuzGeracaoArquivo {
 
     public static void main(String[] args) {
 
         ContratoRepository repo = new ContratoRepository();
-        List<Contrato> contratos = repo.listar();
+        Contrato contrato = repo.exibirContrato();
 
         //gerar o arquivo contendo os contratos
         GeradorArquivo geradorArquivo = new GeradorArquivo();
-        geradorArquivo.csv(contratos);
-        geradorArquivo.txt(contratos);
+        geradorArquivo.csv(contrato);
+        geradorArquivo.txt(contrato);
     }
 }
