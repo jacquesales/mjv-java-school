0b30848725d7cc986ff5d4ed84ce0780d2d85340
Jacqueline Sales <tendencce@gmail.com>
2022-05-05 20:11:06 -0300

    Gerando a notificação ao instanciar contrato via reposiory

--- agua-luz-notification/src/main/java/app/SistemaAguaLuzNotification.java
+++ agua-luz-notification/src/main/java/app/SistemaAguaLuzNotification.java
@@ -1,47 +1,17 @@
 package app;
 
-import domain.Cadastro;
 import domain.Contrato;
-import domain.Endereco;
-import domain.enums.TipoNotificacao;
-import domain.enums.TipoPais;
-import domain.enums.TipoServico;
-import services.GerarNotificacao;
-
-import java.time.LocalDate;
-import java.time.LocalTime;
+import repository.ContratoTxtRepository;
+import services.GeradorNotificacao;
 
 public class SistemaAguaLuzNotification {
 
     public static void main(String[] args) {
 
-        Endereco endereco = new Endereco();
-        endereco.setLogradouro("Rua Anita Malfatti");
-        endereco.setNumero("108");
-        endereco.setComplemento("Casa 2");
-        endereco.setBairro("Centro");
-        endereco.setCidade("São Paulo");
-        endereco.setUf("SP");
-        endereco.setCep("01.311-260");
-        endereco.setPais(TipoPais.BRASIL);
+        ContratoTxtRepository repo = new ContratoTxtRepository();
+        Contrato contrato = repo.buscar();
 
-        Cadastro cadastro = new Cadastro();
-        cadastro.setCpf("123.456.789-00");
-        cadastro.setRg("12345678-9");
-        cadastro.setNome("Maria Julia Santos");
-        cadastro.setCelular(11991234567L);
-        cadastro.setEndereco(endereco);
-
-        Contrato contrato = new Contrato();
-        contrato.setProtocolo(2022025687);
-        contrato.setData(LocalDate.now());
-        contrato.setHora(LocalTime.now());
-        contrato.setTipoServico(TipoServico.AGUA);
-        contrato.setTipoNotificacao(TipoNotificacao.SMS);
-        contrato.setCadastro(cadastro);
-
-        GerarNotificacao formatarNotificacao = new GerarNotificacao();
-        formatarNotificacao.mensagem(contrato);
-
+        GeradorNotificacao gn = new GeradorNotificacao();
+        gn.gerarMensagem(contrato);
     }
 }
