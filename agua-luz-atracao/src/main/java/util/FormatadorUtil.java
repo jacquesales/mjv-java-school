5339c0faaf55f3e38f81a51097bb771bee09219e
Jacqueline Sales <tendencce@gmail.com>
2022-05-06 21:29:44 -0300

    Add novo método para realizar classe de teste

--- agua-luz-atracao/src/main/java/util/FormatadorUtil.java
+++ agua-luz-atracao/src/main/java/util/FormatadorUtil.java
@@ -9,4 +9,13 @@ public class FormatadorUtil {
         String part4 = cpf.substring(9,11);
         return String.format("%s.%s.%s-%s", part1, part2, part3, part4);
     }
+
+    public static String formatarCep(String cep) {
+        cep = String.format("%08d", Long.valueOf(cep));
+
+        String b1 = cep.substring(0,2);
+        String b2 = cep.substring(2,5);
+        String b3 = cep.substring(5,8);
+        return b1+ "." +b2+ "-" +b3;
+    }
 }
